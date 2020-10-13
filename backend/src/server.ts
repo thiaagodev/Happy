081aa1888126678b9417ae31c2fa8063a3749b0f
Thiago Ferreira <thiferr09@gmail.com>
2020-10-13 16:22:56 -0300

    Finalizando o backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import 'express-async-errors';
+
+import path from 'path';
+import cors from 'cors';
 import './database/connection';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-
+app.use('/uploads', express.static(path.join(__dirname,'..', 'uploads')));
+app.use(errorHandler);
+app.use(cors());
+ 
 app.listen(3333);
