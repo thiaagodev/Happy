ffcef3e71ac8677adc2f2047a6db5c06581765ee
Thiago Ferreira <thiferr09@gmail.com>
2020-10-14 13:45:20 -0300

    Finalizando o front end e integraçao com a api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import path from 'path';
-import cors from 'cors';
 import './database/connection';
 
 import routes from './routes';
@@ -10,10 +10,10 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname,'..', 'uploads')));
 app.use(errorHandler);
-app.use(cors());
- 
+
 app.listen(3333);
